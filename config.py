22e45a20868a522365647120e449f903021c0447
𝖎𝖋𝖎 <166911866+ifi0@users.noreply.github.com>
2024-05-05 01:23:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID", "29285293")
-APP_HASH = os.environ.get("APP_HASH", "74b1df0fa75271117354959a68cc2fc6")
+APP_ID = os.environ.get("APP_ID", "29885460")
+APP_HASH = os.environ.get("APP_HASH", "9fece1c7f0ebf1526ed9ade4cb455a03")
 SESSION = os.environ.get("SESSION")
 ha313so = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
 ha313so.start()
